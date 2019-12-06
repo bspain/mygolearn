a671a098673c360f80096cfc4c47c12ccc3b8668
Ben Spain <ben.spain@target.com>
2019-12-06 15:48:14 -0600

    json with volitile type

--- json/json.go
+++ json/json.go
@@ -45,6 +45,23 @@ func main() {
 		fmt.Printf("l: %v\nerr: %v", l, err)
 	}
 
+	fmt.Println("\n\nJSON Array into array of numbers - float64 is base number type")
+	{
+		type List struct {
+			Entries []float64 `json:"entries"`
+		}
+
+		b := []byte(`
+		{
+			"entries" : [ 2, 3, 4 ]
+		}`)
+
+		var l List
+		err := json.Unmarshal(b, &l)
+
+		fmt.Printf("l: %v\nerr: %v", l, err)
+	}
+
 	fmt.Println("\n\nJSON Array of objects into object - converts to map")
 	{
 		type List struct {
@@ -86,7 +103,7 @@ func main() {
 		fmt.Printf("m: %v\nerr: %v", m, err) // 'm: {map[bar:[bar1 bar2] baz:[] foo:[foo1]]}'
 	}
 
-	fmt.Println("\n\nJSON mapping to nested object")
+	fmt.Println("\n\nJSON mapping of an unknown object")
 	{
 		// We have to do some JSON-splunking and reflection to get this object to align with the Unmarsheled object
 		type MyEntries struct {
@@ -101,10 +118,10 @@ func main() {
 		var unk map[string]interface{}
 		err := json.Unmarshal(b, &unk)
 
-		fmt.Printf("unk: %v\nerr: %v\n", unk, err)            // 'unk: map[entries:[2 3 4]]'
+		fmt.Printf("unk: %v\nerr: %v\n", unk, err)                         // 'unk: map[entries:[2 3 4]]'
 		fmt.Printf("unk[\"entries\"]: %v\nerr: %v\n", unk["entries"], err) // 'unk["entries"]: [2 3 4]'
 
-		l := unk["entries"].([]interface{})  // l is a 'generic' array
+		l := unk["entries"].([]interface{})    // l is a 'generic' array
 		fmt.Printf("l: %v\nerr: %v\n", l, err) // 'l: [2 3 4]'
 
 		// There is no easy way to convert the generic []interface{} into []float64, so manually walking the values it is :(
@@ -112,7 +129,110 @@ func main() {
 		for _, e := range l {
 			myEntries.entries = append(myEntries.entries, e.(float64))
 		}
-		
-		fmt.Printf("myEntries: %v\nerr: %v\n", myEntries, err) // 'unTypedEntries: map[entries:[2 3 4]]'
+
+		fmt.Printf("myEntries: %v\nerr: %v\n", myEntries, err) // 'myEntries: {[2 3 4]}'
 	}
+
+	fmt.Println("\n\nJSON mapping of nested object")
+	{
+		// This object is within the JSON structure, but we don't want to type out the whole thing
+		type MyEntries struct {
+			Entries []float64	`json:"entries"`
+		}
+
+		b := []byte(`
+		{
+			"resp": {
+				"list": {
+					"entries" : [ 2, 3, 4 ]
+				}
+			}
+		}`)
+
+		// Treat the Json as a generic map[string] nested set to get to what we want
+		var unk map[string]map[string]interface{}
+		err := json.Unmarshal(b, &unk)
+
+		fmt.Printf("err: %v\n", err)
+		fmt.Printf("unk: %v\n", unk)										// 'unk: map[resp:map[list:map[entries:[2 3 4]]]]'
+		fmt.Printf("unk[\"resp\"]: %v\n", unk["resp"])						// 'unk["resp"]: map[list:map[entries:[2 3 4]]]'
+		fmt.Printf("unk[\"resp\"][\"list\"]: %v\n", unk["resp"]["list"])	// 'unk["resp"]["list"]: map[entries:[2 3 4]]'
+
+		// Re-marshal list back out to JSON
+		l, err2 := json.Marshal(unk["resp"]["list"])						
+
+		fmt.Printf("l: %v\nerr2: %v\n", string(l), err2)					// 'l: {"entries":[2,3,4]}'
+
+		// Now, unmarshal list as the JSON we care about
+		var myEntries MyEntries
+		err3 := json.Unmarshal(l, &myEntries)
+
+		fmt.Printf("myEntries: %v\nerr3: %v\n", myEntries, err3) 			// 'myEntries: {[2 3 4]}'
+	}
+
+	fmt.Println("\n\nJSON decoding of string or string array property")
+	{
+		b := []byte(`
+		{
+			"groups" : [
+				{
+					"suites" : [ "one", "two", "three" ],
+					"alias" : "foo"
+				},
+				{
+					"suites" : [ "four", "five" ],
+					"alias" : [ "bar", "baz" ]		
+				}
+			]
+		}`)
+
+		var l GroupList
+		err := json.Unmarshal(b, &l)
+
+		fmt.Printf("err: %v\n", err)
+		fmt.Printf("l: %v\n", l)		// 'l: {[{[one two three] foo} {[four five] [bar baz]}]}'
+
+		for i, v := range l.Groups {
+
+			// Struct method handles getting alias always as a []string
+			alias := v.GetAlias()
+
+			fmt.Printf("Group# %v\nSuites: %v, Alias: %v\n", i, v.Suites, alias)
+
+			/*
+			Group# 0
+			Suites: [one two three], Alias: [foo]
+			Group# 1
+			Suites: [four five], Alias: [bar baz]
+			*/
+		}
+	}
+}
+
+// Types for JSON decoding example
+
+// Group is a []string of suites and an alias (either string or []string)
+type Group struct {
+	Suites 		[]string				`json:"suites"`
+	Alias		interface{}				`json:"alias"`		// Cound be string or []string
+}
+
+// GroupList is a list of groups
+type GroupList struct {
+	Groups 	[]Group		`json:"groups"`
+}
+
+// GetAlias converts the Alias of a Group into a []string
+func (g *Group) GetAlias() (alias []string) {
+	
+	switch v := g.Alias.(type) {
+	case string: 
+		alias = append(alias, v)
+	case []interface{}:
+		for _, e := range v {
+			alias = append(alias, e.(string))
+		}
+	}
+
+	return
 }
